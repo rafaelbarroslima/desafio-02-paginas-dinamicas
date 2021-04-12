9139f07ab805d84c1e90bdec08cda6c6e03eeb1d
Rafael Barros <62842913+rafaelbarroslima@users.noreply.github.com>
2021-04-12 12:59:53 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports = [
+  {
+    id: "7pqp3IKyC0s",
+    title: "7 Tags HTML que você precisa conhecer | Mayk Brito",
+    duration: "19:30 min",
+    price: "Free",
+    featured: true
+  },
+  {
+    id: "K5yYBJhix5A",
+    title: "Extraindo dados de outros sites com Puppeteer JS | Mayk Brito",
+    duration: "26:49 min",
+    price: "Free"
+  },
+  {
+    id: "vYlz3SmNXQQ",
+    title: "Consumindo API no Front e no Back-end | Mayk Brito",
+    duration: "35:57 min",
+    price: "Free"
+  },
+  {
+    id: "6-VDE3H9-WU",
+    title: "aginação no Front End com JS puro | Mayk Brito",
+    duration: "1:14:49 min",
+    price: "Free",
+    featured: true
+  },
+  {
+    id: "SGwHpzgqzgk",
+    title: "Criando um Carousel do Zero só com CSS | Mayk Brito",
+    duration: "25:18 min",
+    price: "Free"
+  },
+  {
+    id: "wHzpWi3FxI8",
+    title: "Criando barra de percentual de leitura com JS | Mayk Brito",
+    duration: "20:22 min",
+    price: "Free",
+    featured: true
+  }
+]
